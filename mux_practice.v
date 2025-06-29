6d596926f36098f8319c6f617aac740ffdcc68b6
Vivian Zeru <vivian.zeru@gmail.com>
2025-06-29 10:37:22 -0400

    add 9 to 1 mux

--- mux_practice.v
+++ mux_practice.v
@@ -14,3 +14,33 @@ module mux_2to1bus(
     assign out = (sel == 0) ? a : b;
 
 endmodule
+
+module mux_9to1( 
+    input [15:0] a, b, c, d, e, f, g, h, i,
+    input [3:0] sel,
+    output [15:0] out );
+    
+    always @(*) begin
+        if (sel == 0)
+            out = a;
+        else if (sel == 1)
+            out = b;
+        else if (sel == 2)
+            out = c;
+        else if (sel == 3)
+            out = d;
+        else if (sel == 4)
+            out = e;
+        else if (sel == 5)
+            out = f;
+        else if (sel == 6)
+            out = g;
+        else if (sel == 7)
+            out = h;
+        else if (sel == 8)
+            out = i;
+        else
+            out = 16'b1111111111111111;
+    end
+
+endmodule
