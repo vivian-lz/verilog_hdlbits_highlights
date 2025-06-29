a2fe11244505910b04c1cf37a0938f9ad120db93
Vivian Zeru <vivian.zeru@gmail.com>
2025-06-29 10:25:04 -0400

    add bus 2to1 mux

--- mux_practice.v
+++ mux_practice.v
@@ -5,3 +5,12 @@ module mux_2to1(
     assign out = (sel == 0) ? a : b;
 
 endmodule
+
+module mux_2to1bus( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+    
+    assign out = (sel == 0) ? a : b;
+
+endmodule
