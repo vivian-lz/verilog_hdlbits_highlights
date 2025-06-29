1335f0b8c27370db13b2b1d6adceeafbcf112ba2
Vivian Zeru <vivian.zeru@gmail.com>
2025-06-29 12:41:56 -0400

    Update adder_practice.v

--- adder_practice.v
+++ adder_practice.v
@@ -3,7 +3,7 @@
 //One add16 module computes the lower 16 bits of the addition result, while the second add16 module computes 
 //the upper 16 bits of the result. Your 32-bit adder does not need to handle carry-in (assume 0) or carry-out (ignored).
 //Within each add16, 16 full adders (module add1, not provided) are instantiated to actually perform the addition. You must write the full adder module
-module top_module (
+module adder32 (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
@@ -20,23 +20,5 @@ module add1 ( input a, input b, input cin, output sum, output cout );
     assign x = a ^ b;
     assign sum = x ^ cin;
     assign cout = (a & b) | (x & cin);
-
-endmodule
-
-
-//given module add16 ( input[15:0] a, input[15:0] b, input cin, output[15:0] sum, output cout );
-//Instantiate two of them to create a 32-bit adder. One add16 module computes the lower 16 bits of the addition result, 
-//while the second add16 module computes the upper 16 bits of the result, after receiving the carry-out from the first adder. 
-//Your 32-bit adder does not need to handle carry-in (assume 0) or carry-out (ignored), but the internal modules need to 
-//in order to function correctly. (In other words, the add16 module performs 16-bit a + b + cin, while your module performs 
-//32-bit a + b).
-module adder1(
-    input [31:0] a,
-    input [31:0] b,
-    output [31:0] sum
-);
-    wire cout_internal;
-    add16 lower_adder (a[15:0], b[15:0], 1'd0, sum[15:0], cout_internal);
-    add16 higher_adder(a[31:16], b[31:16], cout_internal, sum[31:16]);
 
 endmodule
