0ddd4a1c625822fd155e622cfad5c9530933a6da
Vivian Zeru <vivian.zeru@gmail.com>
2025-06-29 12:21:13 -0400

    combine assign and wires files

--- 7458chip.v
+++ 7458chip.v
@@ -1,4 +1,4 @@
-module top_module ( 
+module wires_method_7458chip ( 
     input p1a, p1b, p1c, p1d, p1e, p1f,
     output p1y,
     input p2a, p2b, p2c, p2d,
@@ -13,5 +13,15 @@ module top_module (
     assign three = p1d & p1e & p1f;
     assign four = p1a & p1b & p1c;
     assign p1y = three | four;
+
+endmodule
+
+module assign_method_7458chip ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+	  assign p2y = (p2a & p2b) | (p2c & p2d);
+    assign p1y = (p1e & p1f & p1d) | (p1a & p1c & p1b);
 
 endmodule
